24378773d6b7c75359e7c39f7e2b18e401146bd7
Mark Bowden <Mbowden01@gmail.com>
2014-07-14 21:41:40 +0900

    add license, comments
    I think this might be useful

--- rulePage.py
+++ rulePage.py
@@ -1,12 +1,39 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+For a copy of the GNU General Public License see http://www.gnu.org/licenses/
+
+Copyright Mark Bowden 2013, 2014
+"""
+
+"""
+a program for ruling pages and some utilities for costumizing
+"""
+
 # as of now the usage of this is copy and paste into scripter console
 
 width, height = getPageSize()
 top_margin, left_margin, right_margin, bottom_margin = getPageMargins()
-#change to absolute positions
-right_margin = width - right_margin
+
+# *this I believe is the way to go with margin getting*
+# change to absolute positions
+# we now have the x position of the right_margin
+# and the y position of bottom margin
+right_margin = width - right_margin 
 bottom_margin = height - bottom_margin
 
+
+# TODO, take a type (dotted, solid) to draw
 def drawLine(y):
+  """draw a line from margin to margin horizontal at height y"""
   name = createLine(left_margin, y, right_margin, y)
   return name
 
@@ -26,7 +53,10 @@ def drawRule(spacing, y):
   drawLine(y)
   return y + spacing
 
+
 def rulePage(spacing, function):
+  """ rulePage takes a function which draws a rule with spacing of "spacing" at position y and returns
+      a new position y to continue ruling from. """
   y = top_margin
   while y < bottom_margin:
     y = function(spacing, y)
@@ -35,4 +65,5 @@ def rulePage(spacing, function):
 def rule10():
   rulePage(10, draw4Rules)
 
+# example usage, write in the functions you want to use or import this module into another program for use
 rule10()
